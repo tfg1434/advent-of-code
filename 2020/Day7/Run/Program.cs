bf241e284bc65eef3bccd182ca4e8c02c616f8d9
torontofangirl <toronto.tristan@gmail.com>
2021-11-28 11:58:40 -0600

    re-moving

--- 2020/Day7/Run/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Reflection;
-using Day7;
-
-Console.WriteLine($"Silver: { Silver.Run(File.ReadAllText(@"in.txt")) }");
-Console.WriteLine($"Gold: { Gold.Run(File.ReadAllText(@"in.txt")) }");
