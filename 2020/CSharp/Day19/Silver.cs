4382e7dbe7a09df8bb471f83dc808783bd1e5dab
torontofangirl <toronto.tristan@gmail.com>
2021-11-29 06:21:16 -0600

    update

--- 2020/CSharp/Day19/Silver.cs
+++ 2020/CSharp/Day19/Silver.cs
@@ -1,33 +1,36 @@
-﻿using FunctionalSharp;
+﻿using System.Data;
+using FunctionalSharp;
 using static FunctionalSharp.F;
 
 namespace Day19;
 
-abstract record class Rule() {
-    public abstract bool IsMatch(string input);
-}
+interface IRule { }
 
-record class ConstantRule(string Match) : Rule {
-    public override bool IsMatch(string input) 
-        => input == Match;
-}
+record struct Constant(char Symbol) : IRule;
 
-record class MultiRule(Lst<Rule> Rules) : Rule {
-    public override bool IsMatch(string input) 
-        => Rules.All(r => r.IsMatch(input));
-}
-
-record class OrRule(Lst<Rule> First, Lst<Rule> Second) : Rule {
-    public override bool IsMatch(string input) 
-        => First.All(r => r.IsMatch(input)) || Second.All(r => r.IsMatch(input));
-}
+record struct RuleRef(int Ref) : IRule;
 
 public static class Silver {
     public static int Run(string input) {
         string[] lines = input.Split(Environment.NewLine);
-        IEnumerable<string> rules = lines.TakeWhile(line => line != "");
+        IEnumerable<string> rulesRaw = lines.TakeWhile(line => line != "");
         IEnumerable<string> messages = lines.SkipWhile(line => line != "").Skip(1);
-
+        IEnumerable<IRule> rules = rulesRaw.Map(ParseRule);
+        
+        
+        
         return 0;
+    }
+    
+    private static Map<int, Lst<IRule>> ParseRules(string input) {
+        return input.Split(Environment.NewLine).Map(line => {
+            string[] split = line.Split(": ");
+            int id = int.Parse(split[0]);
+            
+
+        });
+    }
+
+
     }
 }
