7838cd464b143cc40a75089306b3f3482a3fe371
torontofangirl <toronto.tristan@gmail.com>
2021-11-30 00:06:55 -0600

    started writing match method

--- 2020/CSharp/Day19/Silver.cs
+++ 2020/CSharp/Day19/Silver.cs
@@ -11,7 +11,11 @@ record struct RuleRef(int Ref) : IRule;
 
 public static class Silver {
     public static int Run(string input) {
-        Map<int, IEnumerable<IEnumerable<IRule>>> rules = ParseRules(input);
+        string[] lines = input.Split(Environment.NewLine);
+        Map<int, Lst<Lst<IRule>>> rules = 
+            ParseRules(lines.TakeWhile(line => line != ""));
+        IEnumerable<string> messages = lines.SkipWhile(line => line != "").Skip(1);
+        
         
 
 
@@ -19,17 +23,33 @@ public static class Silver {
         return 0;
     }
     
-    private static Map<int, IEnumerable<IEnumerable<IRule>>> ParseRules(string input) 
-        => input.Split(Environment.NewLine).Map(line => {
+    private static Lst<int> MatchingRules(Lst<Lst<IRule>> rules, int position, int id) {
+        //orRule: inner Lst
+        //rule: inner inner IRule
+
+        rules[id].Map(orRule =>  {
+            var positions = List(position);
+
+            orRule.ForEach(rule => {
+                positions = positions.Map(idx =>
+                    rule switch {
+                        Constant @const when 
+                    })
+            });
+        });
+
+        return default;
+    }
+    
+    private static Map<int, Lst<Lst<IRule>>> ParseRules(IEnumerable<string> lines) 
+        => lines.Map(line => {
             string[] split = line.Split(": ");
             int id = int.Parse(split[0]);
             string[] parts = split[1].Split(" | ");
 
             return (id, parts.Map(side =>
                 side.Split(' ').Map<string, IRule>(part =>
-                    part.StartsWith('"') ? new Constant(part[1]) : new RuleRef(int.Parse(part)))));
-
-
+                    part.StartsWith('"') ? new Constant(part[1]) : new RuleRef(int.Parse(part))).ToLst()).ToLst());
+            
         }).ToMap();
-    }
 }
