d384aace7e2a1cf3f18226e4fe27390d0d867ada
torontofangirl <toronto.tristan@gmail.com>
2021-12-01 04:40:58 -0600

    day 1 gold

---
+++ 2021/Python/Day1/gold.py
@@ -0,0 +1,35 @@
+import os
+__location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
+
+with open(os.path.join(__location__, "in.txt")) as file:
+    lines = file.readlines()
+
+def getWinPos(n: int) -> list[int]:
+    y = n
+
+    list = []
+    for _ in range(3):
+        list.append(y)
+
+        y += 1
+
+    return list
+
+amnt = len(lines) - 2
+lastSum = 999999
+ans = 0
+
+for i in range(amnt):
+    sum = 0
+
+    for j in getWinPos(i):
+        sum += int(lines[j])
+
+    if sum > lastSum:
+        ans += 1
+
+    lastSum = sum
+
+print(ans)
+
+
