5071415d1579e21e07afa429adc535f7933f5230
torontofangirl <toronto.tristan@gmail.com>
2021-12-01 22:47:50 -0600

    setup python

--- 2021/Python/Day2/silver.py
+++ 2021/Python/Day2/silver.py
@@ -4,6 +4,15 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, "in.txt")) as file:
     lines = file.readlines()
 
+prevLine = 999999
+count = 0
 
+for line in lines:
+    line = int(line)
 
+    if line > prevLine:
+        count += 1
 
+    prevLine = line
+
+print(count)
