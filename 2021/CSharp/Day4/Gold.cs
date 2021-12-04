ac57c3d587b38471c308feab01c1b9f242d88a0d
torontofangirl <toronto.tristan@gmail.com>
2021-12-04 00:54:54 -0600

    clean up

--- 2021/CSharp/Day4/Gold.cs
+++ 2021/CSharp/Day4/Gold.cs
@@ -4,24 +4,28 @@ using FunctionalSharp;
 namespace Day4;
 
 public static class Gold {
-    record struct BoardSquare(int Number, bool Marked);
+    private readonly record struct BoardSquare(int Number, bool Marked);
 
-    static Func<bool, int, BoardSquare> OfBoardSquare
-        => (marked, number)  => new(number, marked);
+    private static Func<bool, int, BoardSquare> OfBoardSquare
+        => (marked, number) => new(number, marked);
 
-    static bool CheckForMatch(List<List<BoardSquare>> board) {
-        return //board[0][0] [1][0] [2][0] ... [0][1]
-            Enumerable.Range(0, 5).Map(y => board[y].All(square => square.Marked)).Any(b => b) ||
-            Enumerable.Range(0, 5).Map(x => Enumerable.Range(0, 5).Map(y => board[y][x]).All(square => square.Marked)).Any(b => b);
-        
-    }
-    
+    private static bool CheckForMatch(List<List<BoardSquare>> board)
+        => Enumerable.Range(0, 5)
+               .Map(y => board[y]
+                   .All(square => square.Marked))
+               .Any(b => b) || 
+           //board[0][0] [1][0] [2][0] ... [0][1]
+           Enumerable.Range(0, 5)
+               .Map(x => Enumerable.Range(0, 5)
+                   .Map(y => board[y][x])
+                   .All(square => square.Marked))
+               .Any(b => b);
+
     public static int Run(string input) {
         string[] lines = input.Split(Environment.NewLine);
         IEnumerable<int> numbers = lines[0].Split(',').Map(int.Parse).ToLst();
         int boardsCount = (lines.Length - 1) / 6;
-        List<List<List<BoardSquare>>> boards = new();//new int[boardsCount][Board];
-        int ans;
+        List<List<List<BoardSquare>>> boards = new();
 
         for (int i = 2; i < boardsCount * 6 + 2; i += 6) {
             boards.Add(lines[i..(i + 5)]
@@ -39,17 +43,18 @@ public static class Gold {
                     }
                 }
 
-                if (CheckForMatch(boards[i]))
-                    if (boards.Count == 1)
-                        return Enumerable.Range(0, 5)
-                            .Map(y => boards[i][y].Sum(square => !square.Marked ? square.Number : 0)).Sum() * n;
-                    else {
-                        boards.RemoveAt(i);
-                        i--;
-                    }
+                if (!CheckForMatch(boards[i])) continue;
+
+                if (boards.Count == 1)
+                    return Enumerable.Range(0, 5)
+                        .Map(y => boards[i][y].Sum(square => !square.Marked ? square.Number : 0)).Sum() * n;
+                
+                boards.RemoveAt(i);
+                i--;
             }
         }
 
         return default;
     }
 }
+
--- 2021/CSharp/Day4/Silver.cs
+++ 2021/CSharp/Day4/Silver.cs
@@ -4,24 +4,28 @@ using FunctionalSharp;
 namespace Day4;
 
 public static class Silver {
-    record struct BoardSquare(int Number, bool Marked);
+    private readonly record struct BoardSquare(int Number, bool Marked);
 
-    static Func<bool, int, BoardSquare> OfBoardSquare
-        => (marked, number)  => new(number, marked);
+    private static Func<bool, int, BoardSquare> OfBoardSquare
+        => (marked, number) => new(number, marked);
 
-    static bool CheckForMatch(List<List<BoardSquare>> board) {
-        return //board[0][0] [1][0] [2][0] ... [0][1]
-            Enumerable.Range(0, 5).Map(y => board[y].All(square => square.Marked)).Any(b => b) ||
-            Enumerable.Range(0, 5).Map(x => Enumerable.Range(0, 5).Map(y => board[y][x]).All(square => square.Marked)).Any(b => b);
-        
-    }
-    
+    private static bool CheckForMatch(List<List<BoardSquare>> board)
+        => Enumerable.Range(0, 5)
+               .Map(y => board[y]
+                   .All(square => square.Marked))
+               .Any(b => b) || 
+           //board[0][0] [1][0] [2][0] ... [0][1]
+           Enumerable.Range(0, 5)
+               .Map(x => Enumerable.Range(0, 5)
+                   .Map(y => board[y][x])
+                   .All(square => square.Marked))
+               .Any(b => b);
+
     public static int Run(string input) {
         string[] lines = input.Split(Environment.NewLine);
         IEnumerable<int> numbers = lines[0].Split(',').Map(int.Parse).ToLst();
         int boardsCount = (lines.Length - 1) / 6;
-        List<List<List<BoardSquare>>> boards = new();//new int[boardsCount][Board];
-        int ans;
+        List<List<List<BoardSquare>>> boards = new();
 
         for (int i = 2; i < boardsCount * 6 + 2; i += 6) {
             boards.Add(lines[i..(i + 5)]
@@ -30,21 +34,18 @@ public static class Silver {
         }
 
         foreach (int n in numbers) {
-            for (int i = 0; i < boards.Count; i++) {
-                for (int j = 0; j < boards[i].Count; j++) {
-                    for (int k = 0; k < boards[i][j].Count; k++) {
-                        if (boards[i][j][k].Number == n) {
-                            boards[i][j][k] = OfBoardSquare(true, boards[i][j][k].Number);
+            foreach (List<List<BoardSquare>> board in boards) {
+                foreach (List<BoardSquare> ys in board) {
+                    for (int x = 0; x < ys.Count; x++) {
+                        if (ys[x].Number == n) {
+                            ys[x] = OfBoardSquare(true, ys[x].Number);
                         }
                     }
                 }
 
-                if (CheckForMatch(boards[i])) {
-                    ans = Enumerable.Range(0, 5)
-                        .Map(y => boards[i][y].Sum(square => !square.Marked ? square.Number : 0)).Sum() * n;
-
-                    return ans;
-                }
+                if (CheckForMatch(board))
+                    return Enumerable.Range(0, 5)
+                        .Map(y => board[y].Sum(square => !square.Marked ? square.Number : 0)).Sum() * n;
             }
         }
 
