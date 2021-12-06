81eb8476aeb298aee3481badc181dc27bf65373b
torontofangirl <toronto.tristan@gmail.com>
2021-12-06 00:23:08 -0600

    gold

--- 2021/Python/Day6/gold.py
+++ 2021/Python/Day6/gold.py
@@ -4,3 +4,24 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, "in.txt")) as file:
     lines = file.read().splitlines()
 
+fishes = list(map(int, lines[0].split(",")))
+
+types = [0] * 10
+for fish in fishes:
+    types[fish] += 1
+
+for i in range(256):
+    nts = [0] * 10
+
+    for j in range(len(types) - 1):
+        nts[j] += types[j + 1]
+
+    # fish of type 6 += fish of type 0
+    nts[6] += types[0]
+    #fish of type 8 += fish of type 0
+    nts[8] += types[0]
+
+    types = nts
+
+print(sum(types))
+        
--- 2021/Python/Day6/silver.py
+++ 2021/Python/Day6/silver.py
@@ -4,7 +4,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 with open(os.path.join(__location__, "in.txt")) as file:
     lines = file.read().splitlines()
 
-fishes = list(map(int, lines[0].split(',')))
+fishes = list(map(int, lines[0].split(",")))
 
 for i in range(80):
     toAppend = []
